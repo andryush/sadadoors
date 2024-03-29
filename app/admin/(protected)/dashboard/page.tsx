6b2b76063cb21b76360c28e7ac8eb39a2fa0f5c2
andryush <andryush333@gmail.com>
2024-03-29 05:22:25 +0400

    feat: routing folders structure

---
+++ app/admin/(protected)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <div>(PROTECTED)Dashboard page</div>;
+};
+
+export default Page;
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,37 +1,8 @@
-'use client';
-import { BackgroundBeams } from '@/components/ui/background-beams';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { LabelInputContainer } from '@/components/ui/label-input-container';
-import { BottomGradient } from '@/components/ui/bottom-gradient';
+import { redirect } from 'next/navigation';
 
-export default function Login() {
-  return (
-    <>
-      <div className='relative z-50 mx-auto w-full max-w-md rounded-none bg-white p-4 shadow-input dark:bg-black md:rounded-2xl md:p-8'>
-        <form className='my-8' onSubmit={() => {}}>
-          <LabelInputContainer className='mb-4'>
-            <Label htmlFor='email'>Email</Label>
-            <Input id='email' placeholder='jane@doe.com' type='email' />
-          </LabelInputContainer>
-          <LabelInputContainer className='mb-4'>
-            <Label htmlFor='password'>Password</Label>
-            <Input id='password' placeholder='••••••••' type='password' />
-          </LabelInputContainer>
-          <button
-            className='group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]'
-            type='submit'
-          >
-            Sign in &rarr;
-            <BottomGradient />
-          </button>
+const Page = () => {
+  redirect('/admin/sign-in');
+  return <div>page</div>;
+};
 
-          <div className='my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700' />
-        </form>
-      </div>
-      <div className='z-0'>
-        <BackgroundBeams />
-      </div>
-    </>
-  );
-}
+export default Page;
